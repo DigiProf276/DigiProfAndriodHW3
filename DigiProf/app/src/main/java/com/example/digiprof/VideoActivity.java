81e1a26523250bf45df9d6412d09fcc6bb883eac
Harwinder Dhillon <harwinderdhillon715@gmail.com>
2020-11-08 23:03:11 -0800

    Submit on Monday Nov 09

--- DigiProf/app/src/main/java/com/example/digiprof/VideoActivity.java
+++ DigiProf/app/src/main/java/com/example/digiprof/VideoActivity.java
@@ -1,16 +1,30 @@
 package com.example.digiprof;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class VideoActivity extends AppCompatActivity {
     // UI Views
     FloatingActionButton addVideosBtn;
+    private RecyclerView videosRv;
+    //ArrayList
+    private ArrayList<ModelVideo> videoArrayList;
+    //adapter
+    private AdapterVideo adapterVideo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,10 +33,14 @@ public class VideoActivity extends AppCompatActivity {
         // change actionbar title.
         setTitle("Videos");
 
-        // Inside it UI views
+        // Initialize UI views
         addVideosBtn = findViewById(R.id.addVideosBtn);
+        videosRv = findViewById(R.id.videosRv);
 
-        //Click Button
+        // function call, load videos
+        loadVideosFromFirebase();
+
+        //Handle Click Button
         addVideosBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -31,5 +49,34 @@ public class VideoActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    private void loadVideosFromFirebase() {
+        // initialize Array List before adding data into it
+        videoArrayList = new ArrayList<>();
+
+        //database reference
+        DatabaseReference ref = FirebaseDatabase.getInstance().getReference("Videos");
+        ref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                // clear list before adding data into it
+                for(DataSnapshot ds: snapshot.getChildren()){
+                    //get Data
+                    ModelVideo modelVideo = ds.getValue(ModelVideo.class);
+                    // add model/data into list
+                    videoArrayList.add(modelVideo);
+                }
+                // setup adapter
+                adapterVideo = new AdapterVideo(VideoActivity.this, videoArrayList);
+                //set adapter to recyclerView
+                videosRv.setAdapter(adapterVideo);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
