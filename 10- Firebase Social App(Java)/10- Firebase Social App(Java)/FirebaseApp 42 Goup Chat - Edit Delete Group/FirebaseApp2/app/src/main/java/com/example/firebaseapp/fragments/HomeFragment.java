95a3e98b4ee595ca3fb24e34a6c4364068b82df0
Harwinder Dhillon <harwinderdhillon715@gmail.com>
2020-12-03 19:17:09 -0800

    Hieu Final Merge

---
+++ 10- Firebase Social App(Java)/10- Firebase Social App(Java)/FirebaseApp 42 Goup Chat - Edit Delete Group/FirebaseApp2/app/src/main/java/com/example/firebaseapp/fragments/HomeFragment.java
@@ -0,0 +1,114 @@
+package com.example.firebaseapp.fragments;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.core.view.MenuItemCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.SearchView;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+//import com.example.firebaseapp.AddPostActivity;
+import com.example.firebaseapp.MainActivity;
+import com.example.firebaseapp.R;
+//import com.example.firebaseapp.SettingsActivity;
+//import com.example.firebaseapp.adapters.AdapterPosts;
+//import com.example.firebaseapp.models.ModelPost;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+    //firebase auth
+    FirebaseAuth firebaseAuth;
+
+    RecyclerView recyclerView;
+//    List<ModelPost> postList;
+//    AdapterPosts adapterPosts;
+
+    public HomeFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view =  inflater.inflate(R.layout.fragment_home, container, false);
+
+        //init
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
+//LinearLayoutManager layoutManager = new LinearLayoutManager(getActivity());
+
+
+        return view;
+    }
+
+
+
+    private void checkUserStatus(){
+        //get current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        if (user != null){
+
+        }
+        else {
+            //user not signed in, go to main acitivity
+            startActivity(new Intent(getActivity(), MainActivity.class));
+            getActivity().finish();
+        }
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        setHasOptionsMenu(true);//to show menu option in fragment
+        super.onCreate(savedInstanceState);
+    }
+
+    /*inflate options menu*/
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        //inflating menu
+        inflater.inflate(R.menu.menu_main, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    /*handle menu item clicks*/
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //get item id
+        int id = item.getItemId();
+        if (id == R.id.action_logout){
+            firebaseAuth.signOut();
+            checkUserStatus();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
